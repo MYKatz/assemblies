3fa4177ee5624f58f13fde5ee29d64191dd14202
Katz <mykatz01@gmail.com>
2021-01-26 19:21:16 -0800

    main layout view

--- packages/react-app/src/App.jsx
+++ packages/react-app/src/App.jsx
@@ -128,218 +128,243 @@ function App(props) {
     setRoute(window.location.pathname);
   }, [setRoute]);
 
+  const [show, setShow] = useState(false);
+  const [profile, setProfile] = useState(false);
+  const [menu, setMenu] = useState(false);
+  const [menu1, setMenu1] = useState(false);
+  const [menu2, setMenu2] = useState(false);
+  const [menu3, setMenu3] = useState(false);
+
   return (
-    <div className="App">
-      <Header setRoute={setRoute} />
-
-      <BrowserRouter>
-        <Menu style={{ textAlign: "center" }} selectedKeys={[route]} mode="horizontal">
-          <Menu.Item key="/">
-            <Link
-              onClick={() => {
-                setRoute("/");
-              }}
-              to="/"
-            >
-              Feed
-            </Link>
-          </Menu.Item>
-          <Menu.Item key="/gov">
-            <Link
-              onClick={() => {
-                setRoute("/gov");
-              }}
-              to="/gov"
-            >
-              Gov
-            </Link>
-          </Menu.Item>
-          <Menu.Item key="/contract">
-            <Link
-              onClick={() => {
-                setRoute("/contract");
-              }}
-              to="/contract"
-            >
-              Contract
-            </Link>
-          </Menu.Item>
-          <Menu.Item key="/hints">
-            <Link
-              onClick={() => {
-                setRoute("/hints");
-              }}
-              to="/hints"
-            >
-              Hints
-            </Link>
-          </Menu.Item>
-          <Menu.Item key="/exampleui">
-            <Link
-              onClick={() => {
-                setRoute("/exampleui");
-              }}
-              to="/exampleui"
-            >
-              ExampleUI
-            </Link>
-          </Menu.Item>
-          <Menu.Item key="/subgraph">
-            <Link
-              onClick={() => {
-                setRoute("/subgraph");
-              }}
-              to="/subgraph"
-            >
-              Subgraph
-            </Link>
-          </Menu.Item>
-        </Menu>
-
-        <Switch>
-          <Route exact path="/">
-            <Feed
-              name="YourContract"
-              signer={userProvider.getSigner()}
-              provider={localProvider}
-              address={address}
-              blockExplorer={blockExplorer}
-            />
-          </Route>
-          <Route exact path="/gov">
-            <Gov
-              name="YourContract"
-              signer={userProvider.getSigner()}
-              provider={localProvider}
-              userAddress={address}
-              blockExplorer={blockExplorer}
-            />
-          </Route>
-          <Route exact path="/contract">
-            {/*
-                🎛 this scaffolding is full of commonly used components
-                this <Contract/> component will automatically parse your ABI
-                and give you a form to interact with it locally
-            */}
-            <Contract
-              name="YourContract"
-              signer={userProvider.getSigner()}
-              provider={localProvider}
-              address={address}
-              blockExplorer={blockExplorer}
-            />
-            <Contract
-              name="Dictatorship"
-              signer={userProvider.getSigner()}
-              provider={localProvider}
-              address={address}
-              blockExplorer={blockExplorer}
-            />
-            {/* Uncomment to display and interact with an external contract (DAI on mainnet):
-            <Contract
-              name="DAI"
-              customContract={mainnetDAIContract}
-              signer={userProvider.getSigner()}
-              provider={mainnetProvider}
-              address={address}
-              blockExplorer={blockExplorer}
-            />
-            */}
-          </Route>
-          <Route path="/hints">
-            <Hints
-              address={address}
-              yourLocalBalance={yourLocalBalance}
-              mainnetProvider={mainnetProvider}
-              price={price}
-            />
-          </Route>
-          <Route path="/exampleui">
-            <ExampleUI
-              address={address}
-              userProvider={userProvider}
-              mainnetProvider={mainnetProvider}
-              localProvider={localProvider}
-              yourLocalBalance={yourLocalBalance}
-              price={price}
-              tx={tx}
-              writeContracts={writeContracts}
-              readContracts={readContracts}
-              purpose={purpose}
-              setPurposeEvents={setPurposeEvents}
-            />
-          </Route>
-          <Route path="/subgraph">
-            <Subgraph
-              subgraphUri={props.subgraphUri}
-              tx={tx}
-              writeContracts={writeContracts}
-              mainnetProvider={mainnetProvider}
-            />
-          </Route>
-        </Switch>
-      </BrowserRouter>
-
-      {/* 👨‍💼 Your account is in the top right with a wallet at connect options */}
-      {/* <div style={{ position: "fixed", textAlign: "right", right: 0, top: 0, padding: 10 }}>
-        <Account
-          address={address}
-          localProvider={localProvider}
-          userProvider={userProvider}
-          mainnetProvider={mainnetProvider}
-          price={price}
-          web3Modal={web3Modal}
-          loadWeb3Modal={loadWeb3Modal}
-          logoutOfWeb3Modal={logoutOfWeb3Modal}
-          blockExplorer={blockExplorer}
-        />
-      </div> */}
-
-      {/* 🗺 Extra UI like gas price, eth price, faucet, and support: */}
-      <div style={{ position: "fixed", textAlign: "left", left: 0, bottom: 20, padding: 10 }}>
-        <Row align="middle" gutter={[4, 4]}>
-          <Col span={8}>
-            <Ramp price={price} address={address} />
-          </Col>
-
-          <Col span={8} style={{ textAlign: "center", opacity: 0.8 }}>
-            <GasGauge gasPrice={gasPrice} />
-          </Col>
-          <Col span={8} style={{ textAlign: "center", opacity: 1 }}>
-            <Button
-              onClick={() => {
-                window.open("https://t.me/joinchat/KByvmRe5wkR-8F_zz6AjpA");
-              }}
-              size="large"
-              shape="round"
-            >
-              <span style={{ marginRight: 8 }} role="img" aria-label="support">
-                💬
-              </span>
-              Support
-            </Button>
-          </Col>
-        </Row>
-
-        <Row align="middle" gutter={[4, 4]}>
-          <Col span={24}>
-            {
-              /*  if the local provider has a signer, let's show the faucet:  */
-              localProvider &&
-              localProvider.connection &&
-              localProvider.connection.url &&
-              localProvider.connection.url.indexOf("localhost") >= 0 &&
-              !process.env.REACT_APP_PROVIDER &&
-              price > 1 ? (
-                <Faucet localProvider={localProvider} price={price} ensProvider={mainnetProvider} />
-              ) : (
-                ""
-              )
+    <>
+      <div className="w-full h-full">
+        <div className="flex flex-no-wrap">
+          {/* Sidebar starts */}
+          <div className="absolute lg:relative w-64 h-screen shadow bg-white hidden lg:block">
+            <div className="h-16 w-full flex items-center px-8 font-mono text-2xl">
+              <span className="pt-4">Ecclesia</span>
+            </div>
+            <ul aria-orientation="vertical" className="bg-white rounded py-6">
+              <li className="pl-6 cursor-pointer text-white text-sm leading-3 tracking-normal pb-4 pt-5 bg-indigo-700 focus:text-indigo-700 focus:outline-none">
+                <div className="flex items-center">Explore</div>
+              </li>
+              <li className="pl-6 cursor-pointer text-gray-600 text-sm leading-3 tracking-normal mt-4 mb-4 py-2 hover:text-indigo-700 focus:text-indigo-700 focus:outline-none">
+                <div className="flex items-center">
+                  <span className="ml-2">Governance</span>
+                </div>
+              </li>
+              <li className="pl-6 cursor-pointer text-gray-600 text-sm leading-3 tracking-normal mb-4 py-2 hover:text-indigo-700 focus:text-indigo-700 focus:outline-none">
+                <div className="flex items-center">
+                  <span className="ml-2">Factory</span>
+                </div>
+              </li>
+            </ul>
+          </div>
+          {/*Mobile responsive sidebar*/}
+          <div
+            className={
+              show
+                ? "w-full h-full absolute z-40  transform  translate-x-0 "
+                : "   w-full h-full absolute z-40  transform -translate-x-full"
             }
-          </Col>
-        </Row>
+            id="mobile-nav"
+          >
+            <div className="bg-gray-800 opacity-50 absolute h-full w-full lg:hidden" onClick={() => setShow(!show)} />
+            <div className="absolute z-40 sm:relative w-64 overflow-y-auto md:w-96 shadow pb-4 bg-white lg:hidden transition duration-150 ease-in-out h-full">
+              <div className="flex flex-col justify-between h-full w-full">
+                <div>
+                  <div className="flex items-center justify-between px-8">
+                    <div className="h-16 w-full flex items-center pt-8">Ecclesia</div>
+                    <div
+                      id="closeSideBar"
+                      className="flex items-center justify-center h-10 w-10"
+                      onClick={() => setShow(!show)}
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="icon icon-tabler icon-tabler-x"
+                        width={20}
+                        height={20}
+                        viewBox="0 0 24 24"
+                        strokeWidth="1.5"
+                        stroke="currentColor"
+                        fill="none"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      >
+                        <path stroke="none" d="M0 0h24v24H0z" />
+                        <line x1={18} y1={6} x2={6} y2={18} />
+                        <line x1={6} y1={6} x2={18} y2={18} />
+                      </svg>
+                    </div>
+                  </div>
+                  <ul aria-orientation="vertical" className="bg-white rounded py-6">
+                    <li
+                      className="pl-6 cursor-pointer text-white text-sm leading-3 tracking-normal pb-4 pt-5 bg-indigo-700 focus:text-indigo-700 focus:outline-none"
+                      onClick={() => setMenu(!menu)}
+                    >
+                      <div className="flex items-center">Explore</div>
+                    </li>
+                    <li
+                      className="pl-6 cursor-pointer text-gray-600 text-sm leading-3 tracking-normal mt-4 mb-4 py-2 hover:text-indigo-700 focus:text-indigo-700 focus:outline-none"
+                      onClick={() => setMenu1(!menu1)}
+                    >
+                      <div className="flex items-center">Governance</div>
+                    </li>
+                    <li
+                      className="pl-6 cursor-pointer text-gray-600 text-sm leading-3 tracking-normal mb-4 py-2 hover:text-indigo-700 focus:text-indigo-700 focus:outline-none"
+                      onClick={() => setMenu2(!menu2)}
+                    >
+                      <div className="flex items-center">Factory</div>
+                    </li>
+                  </ul>
+                </div>
+                <div className="w-full">
+                  <div className="flex justify-center mb-4 w-full px-6"></div>
+                  <div className="border-t border-gray-300">
+                    <div className="w-full flex items-center justify-between px-6 pt-1">
+                      <div className="flex items-center">
+                        <img
+                          alt="profile-pic"
+                          src="https://tuk-cdn.s3.amazonaws.com/assets/components/boxed_layout/bl_1.png"
+                          className="w-8 h-8 rounded-md"
+                        />
+                        <p className="md:text-xl text-gray-800 text-base leading-4 ml-2">Jane Doe</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/*Mobile responsive sidebar*/}
+          {/* Sidebar ends */}
+          <div className="w-full">
+            {/* Navigation starts */}
+            <nav className="h-16 flex items-center lg:items-stretch justify-end lg:justify-between bg-white shadow relative z-10">
+              <div className="hidden lg:flex w-full pr-6">
+                <div className="w-1/2 h-full hidden lg:flex items-center pl-6 pr-24"></div>
+                <div className="w-1/2 hidden lg:flex">
+                  <div className="w-full flex items-center pl-8 justify-end">
+                    <div className="flex items-center relative cursor-pointer" onClick={() => setProfile(!profile)}>
+                      <div className="rounded-full">
+                        {profile ? (
+                          <ul className="p-2 w-full border-r bg-white absolute rounded left-0 shadow mt-12 sm:mt-16 ">
+                            <li className="flex w-full justify-between text-gray-600 hover:text-indigo-700 cursor-pointer items-center">
+                              <div className="flex items-center">
+                                <svg
+                                  xmlns="http://www.w3.org/2000/svg"
+                                  className="icon icon-tabler icon-tabler-user"
+                                  width={18}
+                                  height={18}
+                                  viewBox="0 0 24 24"
+                                  strokeWidth="1.5"
+                                  stroke="currentColor"
+                                  fill="none"
+                                  strokeLinecap="round"
+                                  strokeLinejoin="round"
+                                >
+                                  <path stroke="none" d="M0 0h24v24H0z" />
+                                  <circle cx={12} cy={7} r={4} />
+                                  <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
+                                </svg>
+                                <span className="text-sm ml-2">My Profile</span>
+                              </div>
+                            </li>
+                            <li className="flex w-full justify-between text-gray-600 hover:text-indigo-700 cursor-pointer items-center mt-2">
+                              <div className="flex items-center">
+                                <svg
+                                  xmlns="http://www.w3.org/2000/svg"
+                                  className="icon icon-tabler icon-tabler-logout"
+                                  width={20}
+                                  height={20}
+                                  viewBox="0 0 24 24"
+                                  strokeWidth="1.5"
+                                  stroke="currentColor"
+                                  fill="none"
+                                  strokeLinecap="round"
+                                  strokeLinejoin="round"
+                                >
+                                  <path stroke="none" d="M0 0h24v24H0z" />
+                                  <path d="M14 8v-2a2 2 0 0 0 -2 -2h-7a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h7a2 2 0 0 0 2 -2v-2" />
+                                  <path d="M7 12h14l-3 -3m0 6l3 -3" />
+                                </svg>
+                                <span className="text-sm ml-2">Sign out</span>
+                              </div>
+                            </li>
+                          </ul>
+                        ) : (
+                          ""
+                        )}
+                        <div className="relative">
+                          <img
+                            className="rounded-full h-10 w-10 object-cover"
+                            src="https://tuk-cdn.s3.amazonaws.com/assets/components/sidebar_layout/sl_1.png"
+                            alt="avatar"
+                          />
+                          <div className="w-2 h-2 rounded-full bg-green-400 border border-white absolute inset-0 mb-0 mr-0 m-auto" />
+                        </div>
+                      </div>
+                      <p className="text-gray-800 text-sm mx-3">Jane Doe</p>
+                      <div className="cursor-pointer text-gray-600">
+                        <svg
+                          aria-haspopup="true"
+                          xmlns="http://www.w3.org/2000/svg"
+                          className="icon icon-tabler icon-tabler-chevron-down"
+                          width={20}
+                          height={20}
+                          viewBox="0 0 24 24"
+                          strokeWidth="1.5"
+                          stroke="currentColor"
+                          fill="none"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                        >
+                          <path stroke="none" d="M0 0h24v24H0z" />
+                          <polyline points="6 9 12 15 18 9" />
+                        </svg>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="text-gray-600 mr-8 visible lg:hidden relative" onClick={() => setShow(!show)} id="menu">
+                {show ? (
+                  " "
+                ) : (
+                  <svg
+                    aria-label="Main Menu"
+                    aria-haspopup="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="icon icon-tabler icon-tabler-menu cursor-pointer"
+                    width={30}
+                    height={30}
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    fill="none"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  >
+                    <path stroke="none" d="M0 0h24v24H0z" />
+                    <line x1={4} y1={8} x2={20} y2={8} />
+                    <line x1={4} y1={16} x2={20} y2={16} />
+                  </svg>
+                )}
+              </div>
+            </nav>
+            {/* Navigation ends */}
+            {/* Remove class [ h-64 ] when adding a card block */}
+            <div className="container mx-auto py-10 h-64 md:w-4/5 w-11/12 px-6">
+              {/* Remove class [ border-dashed border-2 border-gray-300 ] to remove dotted border */}
+              <div className="w-full h-full rounded border-dashed border-2 border-gray-300">
+                {/* Place your content here */}
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
